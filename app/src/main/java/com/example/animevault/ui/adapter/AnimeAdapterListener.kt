bd001489e42bef8ecd6f27bcb249a20586a30ed5
althof-z <althof.zpv@gmail.com>
2024-07-19 22:06:11 +0700

    use linter guide

--- app/src/main/java/com/example/animevault/ui/adapter/AnimeAdapterListener.kt
+++ app/src/main/java/com/example/animevault/ui/adapter/AnimeAdapterListener.kt
@@ -1,7 +1,6 @@
 package com.example.animevault.ui.adapter
 
 import com.example.domain.model.Anime
-import com.example.domain.model.AnimeHome
 
 interface AnimeAdapterListener {
     fun onClickFavButton(data: Anime)
